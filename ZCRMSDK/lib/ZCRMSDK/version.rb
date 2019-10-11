9370c19e8f1189f00e678f25d1173cb8085d8ff1
pravesh.a <pravesh.a@zohocorp.com>
2019-10-11 12:34:23 +0530

    removing the directory

--- ZCRMSDK/lib/ZCRMSDK/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module ZCRMSDK
-  VERSION = '0.0.1'
-end
--- ZCRMSDK/spec/ZCRMSDK_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe ZCRMSDK do
-  it 'has a version number' do
-    expect(ZCRMSDK::VERSION).not_to be nil
-  end
-end
